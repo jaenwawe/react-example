f5101e5b8e92c4ea980cc9f4fa5f24c607ba6117
Mitch Goshorn <msgoshorn@gmail.com>
2019-07-23 16:56:43 -0400

    Initial commit of demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FirstComponent } from './components/my-first-component';
 
+
+/*
+  Components
+  Components makeup the abstract structure from which an application can be built.
+  Components are intended to be reusable, flexible sections of a website, that
+  maintain their own state and behavior.
+
+  There are primarily two ways to define a component.  The first, is by extending
+  the React.Component class. The second is a "Function Component" which is used
+  for simpler components that don't need any additional behavior.
+
+  All components require method: render
+
+  The render method returns an HTML-like syntax that is used to then
+  render the component by React. Function components are essentially components
+  that only have a render method.
+*/
 const App: React.FC = () => {
   return (
     <div className="App">
+      <FirstComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <FirstComponent />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello React!
+          <FirstComponent />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FirstComponent />
       </header>
+
+      <FirstComponent />
     </div>
   );
 }
---
+++ src/components/my-first-component.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class FirstComponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Hello everyone!</h3>
+                <p>This is my first component!</p>
+            </div>
+        );
+    }
+}

8edd0536d75d6b90ed0e9040b0b75681b4c71831
AmadoMiguel <amadodevia@gmail.com>
2019-07-24 18:17:02 -0500

    Added Miguel's component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 import { FirstComponent } from './components/my-first-component';
 import SecondComponent from './components/my-second-component';
 import NavComponent from './components/nav-component';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
 import { ClickerComponent } from './components/clicker-component';
-import {SpinningAtom} from './components/spinning-atom';
 import BallComponent from './components/ball-component';
 import { dieRollerComponent } from './components/dice-roller-component';
 import JessicaComponent from './components/jessicas-component';
 import FunComponent from './components/fun-page-component';
+import { MiguelComponent } from './components/miguel-component';
 
 /*
   Components
@@ -43,11 +43,11 @@ const App: React.FC = () => {
           <Route path="/first" component={FirstComponent} />
           <Route path="/second" component={SecondComponent} />
           <Route path="/clicker" component={ClickerComponent} />
-          <Route path="/spinning-atom" component={SpinningAtom}/>
           <Route path="/ball" component={BallComponent} />
           <Route path="/dieRoller" component={dieRollerComponent} />
           <Route path="/Jessica" component={JessicaComponent} />
           <Route path="/fungame" component={FunComponent} />
+          <Route path="/miguel" component={MiguelComponent} />
           <Route component={FirstComponent} />
         </Switch>
       </div>

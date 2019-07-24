faa4bd00b07d647ff42e6e0d4fccacbe54c44b98
Kyle Kirsten <kylejkirsten@gmail.com>
2019-07-24 15:31:43 -0400

    All for a really fun game

--- src/components/nav-component.tsx
+++ src/components/nav-component.tsx
@@ -29,6 +29,10 @@ const NavComponent: React.FC = () => {
                             <Link to="/clicker"
                                 className="unset-anchor nav-link">Clicker</Link>
                         </li>
+                        <li className="nav-item active">
+                            <Link to="/fungame"
+                                className="unset-anchor nav-link">Really Fun Game</Link>
+                        </li>
 
                     </ul>
                 </div>

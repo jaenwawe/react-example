f99408215a398fe8d1cb0df46cc2f55a09083d2c
JonLucasWard <jonlucasward@gmail.com>
2019-07-24 15:37:53 -0400

    Cmon!

--- src/components/nav-component.tsx
+++ src/components/nav-component.tsx
@@ -29,6 +29,10 @@ const NavComponent: React.FC = () => {
                             <Link to="/clicker"
                                 className="unset-anchor nav-link">Clicker</Link>
                         </li>
+                        <li className="nav-item active">
+                            <Link to="/dieRoller"
+                                className="unset-anchor nav-link">Roller</Link>
+                        </li>
 
                     </ul>
                 </div>
